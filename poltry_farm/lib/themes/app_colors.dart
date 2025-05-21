83f13615ff429ec46b337571e35c9f40c092d6e9
Khoi Vo <khoi.vo@asnet.com.vn>
2025-05-21 17:43:50 +0700

    apply app colors import assets apply l10n

---
+++ poltry_farm/lib/themes/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:poltry_farm/themes/app_palette.dart';
+
+class PfColors {
+  static ColorScheme light = ColorScheme(
+    brightness: Brightness.light,
+    primary: PfPalette.yellow[5]!,
+    onPrimary: Colors.white,
+    secondary: PfPalette.green[5]!,
+    onSecondary: Colors.white,
+    error: PfPalette.red[5]!,
+    onError: Colors.white,
+    surface: PfPalette.grey[4]!,
+    onSurface: PfPalette.grey[5]!,
+    onSurfaceVariant: PfPalette.grey[10],
+  );
+}
---
+++ poltry_farm/lib/themes/app_palette.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PfPalette {
+  static const MaterialColor yellow = MaterialColor(0xFFFFC100, <int, Color>{
+    5: Color(0xFFFFC100),
+  });
+  static const MaterialColor green = MaterialColor(0xFF28B446, <int, Color>{
+    5: Color(0xFF28B446),
+  });
+  static const MaterialColor red = MaterialColor(0xFFF14336, <int, Color>{
+    5: Color(0xFFF14336),
+  });
+
+  static const MaterialColor grey = MaterialColor(0xFF8391A1, <int, Color>{
+    2: Color(0xFFE8ECF4),
+    4: Color(0xFFF7F8F9),
+    5: Color(0xFF8391A1),
+    10: Color(0xFF000000),
+  });
+}
